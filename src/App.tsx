577d2c405664e4b14633c9b8aa81982e3eff7e01
Ajmir Raziqi <ajmir.ng3@gmail.com>
2025-10-04 17:06:50 +0300

    add sounds

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import "./App.css";
 import KeyPad from "./KeyPad";
 import useKeyListener from "./utils/useKeyListener";
+import { KEYS, KITS } from "./constants";
+import { useState } from "react";
+
+function play(path: string) {
+  const sound = new Audio(path);
+  sound.play();
+}
 
 function App() {
+  const [kitIndex, setKitIndex] = useState(0);
+
+  function changeKit() {
+    setKitIndex(kitIndex + 1);
+    if (KITS.length - 1 === kitIndex) setKitIndex(0);
+    console.log(KITS[kitIndex].name);
+  }
+
   const handleKeyPress = (key: string) => {
-    console.log({ key });
+    if (key === "-") return changeKit();
+
+    // @ts-ignore
+    const sound = KITS[kitIndex][key] as undefined | string;
+    if (sound) play(sound);
   };
   const pressedKeys = useKeyListener(handleKeyPress);
   return <KeyPad pressedKeys={pressedKeys} handleKeyPress={handleKeyPress} />;
